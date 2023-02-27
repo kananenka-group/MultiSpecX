0e529dfc16ef08db69d5c516e7caeae102c14cc2
Alexei Kananenka <akanane@udel.edu>
2023-02-27 17:26:44 -0600

    created files

---
+++ multispecx/__init__.py
@@ -0,0 +1,8 @@
+from multispecx import spectrum
+
+__version__ = "0.1.0"
+__author__ = 'Alexei A. Kananenka, University of Delaware'
+
+
+
+
---
+++ multispecx/spectrum/__init__.py
@@ -0,0 +1,8 @@
+"""
+Main module for converting excitonic Hamiltonian and dipole/polarizability
+trajectories into spectra
+
+"""
+
+class
+
