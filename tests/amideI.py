5a1b058682494a1ef138c6f547752b33ae5bbb3e
Alexei Kananenka <akanane@udel.edu>
2023-03-13 14:20:13 -0500

    added isotope labels into input line

--- tests/amideI.py
+++ tests/amideI.py
@@ -5,7 +5,7 @@ from multispecx import amideI
 
 itp_files = ["topol_Protein1.itp","topol_Protein2.itp","topol_Protein3.itp","topol_Protein4.itp","topol_DPPC.itp","topol_POTion.itp","topol_bindion.itp","topol_CLion.itp","spce.itp"]
 
-s = amideI.AmideI(itp=itp_files,top="topol.top",gro="s0s2s3.gro")
+s = amideI.AmideI(itp=itp_files,top="topol.top",gro="s0s2s3.gro",isotope_labels=['VAL76'])
 s.generateHamiltonian()
 
 
