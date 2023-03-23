9d3baf58fefd31979e4e09669cf6595f342ad2c1
Alexei Kananenka <akanane@udel.edu>
2023-03-23 17:18:39 -0500

    adjusted input parameters for the map generation

--- tests/map/run.py
+++ tests/map/run.py
@@ -3,6 +3,7 @@ import os
 sys.path.append("../..")
 from multispecx import mapbuilder
 
-s = mapbuilder.Mapbuilder(itp=["etoac.itp","tip4p.itp"],top="etoac.top",gro="prod.gro",method="B3LYP",basis="6-311++G(d,p)",vib="normal",nframes=2,xyz=["etoac.xyz","water.xyz"],software="Gaussian",wdir="~/Documents/Research/ester/",xtc="prod.xtc")
+work_dir = "/Users/akanane/Documents/Research/ester"
+s = mapbuilder.Mapbuilder(itp=["etoac.itp","tip4p.itp"],top="etoac.top",gro="prod.gro",method="B3LYP",basis="6-311++G(d,p)",vib="normal",nframes=2,xyz=["etoac.xyz","water.xyz"],software="Gaussian",wdir=work_dir,xtc="prod.xtc")
 s.createJobs()
 
