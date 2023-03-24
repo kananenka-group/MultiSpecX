b3de1a9ed4d8b9a6bd05be2581e8802cf10a1be6
Alexei Kananenka <akanane@udel.edu>
2023-03-24 11:54:56 -0500

    print solvent atoms

--- tests/map/run.py
+++ tests/map/run.py
@@ -4,6 +4,8 @@ sys.path.append("../..")
 from multispecx import mapbuilder
 
 work_dir = "/Users/akanane/Documents/Research/ester"
-s = mapbuilder.Mapbuilder(itp=["etoac.itp","tip4p.itp"],top="etoac.top",gro="prod.gro",method="B3LYP",basis="6-311++G(d,p)",vib="normal",nframes=2,xyz=["etoac.xyz","water.xyz"],software="Gaussian",wdir=work_dir,xtc="prod.xtc")
+transform = [['center',3]]
+
+s = mapbuilder.Mapbuilder(itp=["etoac.itp","tip4p.itp"],top="etoac.top",gro="prod.gro",method="B3LYP",basis="6-311++G(d,p)",vib="normal",nframes=2,xyz=["etoac.xyz","water.xyz"],software="Gaussian",wdir=work_dir,xtc="prod.xtc",transform=transform)
 s.createJobs()
 
