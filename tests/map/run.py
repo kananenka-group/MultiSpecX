aea713a343dd95bc349e1f7cd72d8e20812563e7
Alexei Kananenka <akanane@udel.edu>
2023-03-27 14:00:11 -0500

    updated input file

--- tests/map/run.py
+++ tests/map/run.py
@@ -5,7 +5,10 @@ from multispecx import mapbuilder
 
 work_dir = "/Users/akanane/Documents/Research/ester"
 transform = [['center',3],['rotate',3,4,'x'],['rotate',3,1,'y']]
+basis="Def2TZVP"
+nframes=2
+cut_off1=5.0
 
-s = mapbuilder.Mapbuilder(itp=["etoac.itp","tip4p.itp"],top="etoac.top",gro="prod.gro",method="B3LYP",basis="6-311++G(d,p)",vib="normal",nframes=2,xyz=["etoac.xyz","water.xyz"],software="Gaussian",wdir=work_dir,xtc="prod.xtc",transform=transform,solv_ref_atom="OW",solu_ref_atom="C4",ncores=40)
+s = mapbuilder.Mapbuilder(itp=["etoac.itp","tip4p.itp"],top="etoac.top",gro="prod.gro",method="B3LYP",basis=basis,vib="normal",nframes=nframes,xyz=["etoac.xyz","water.xyz"],software="Gaussian",wdir=work_dir,xtc="prod.xtc",transform=transform,solv_ref_atom="OW",solu_ref_atom="C4",ncores=40,cut_off1=cut_off1)
 s.createJobs()
 
