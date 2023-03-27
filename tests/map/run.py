6db00ed25117c0405629e399ee2d470bf6101a8e
Alexei Kananenka <akanane@udel.edu>
2023-03-27 11:22:48 -0500

    added ncores to input file

--- tests/map/run.py
+++ tests/map/run.py
@@ -6,6 +6,6 @@ from multispecx import mapbuilder
 work_dir = "/Users/akanane/Documents/Research/ester"
 transform = [['center',3],['rotate',3,4,'x'],['rotate',3,1,'y']]
 
-s = mapbuilder.Mapbuilder(itp=["etoac.itp","tip4p.itp"],top="etoac.top",gro="prod.gro",method="B3LYP",basis="6-311++G(d,p)",vib="normal",nframes=2,xyz=["etoac.xyz","water.xyz"],software="Gaussian",wdir=work_dir,xtc="prod.xtc",transform=transform,solv_ref_atom="OW",solu_ref_atom="C4")
+s = mapbuilder.Mapbuilder(itp=["etoac.itp","tip4p.itp"],top="etoac.top",gro="prod.gro",method="B3LYP",basis="6-311++G(d,p)",vib="normal",nframes=2,xyz=["etoac.xyz","water.xyz"],software="Gaussian",wdir=work_dir,xtc="prod.xtc",transform=transform,solv_ref_atom="OW",solu_ref_atom="C4",ncores=40)
 s.createJobs()
 
