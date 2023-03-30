6f37d6873eb75c5d524b3e92a1d1dbfa3c5ec3ef
Alexei Kananenka <akanane@udel.edu>
2023-03-29 19:03:35 -0500

    added new input parameters

--- multispecx/ester/ester.py
+++ multispecx/ester/ester.py
@@ -5,8 +5,9 @@ from dataclasses import dataclass, field
 @dataclass
 class Ester:
    type: list = field(default_factory=lambda: ['dipole'])
-   isotope_labels: list = field(default_factory=lambda: ['none'])
    itp: list = field(default_factory=lambda: ['topol.itp'])
+   ester_unit: list = field(default_factory=lambda: ['C','O','C','O'])
+   gro:  str = "confout.gro"
    xtc:  str = "traj.xtc" 
    top:  str = "topology.top"
    nframes: int = 1
@@ -17,4 +18,6 @@ class Ester:
    def generateHamiltonian(self): 
      # create a system object
      s = System(self.itp)
-     s.readITP()
+     self.atoms, self.molecules, self.atoms_in_mol, _, _ = s.readITP()
+
+     # find indices of ester units in the system
