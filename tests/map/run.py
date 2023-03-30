0e51d4d4b3e38d1da4cc1ea9ece4f6937530cc28
Alexei Kananenka <akanane@udel.edu>
2023-03-30 12:37:14 -0500

    cleaned up, moved amideI example to amideI dir

--- tests/map/run.py
+++ tests/map/run.py
@@ -4,12 +4,12 @@ sys.path.append("../..")
 from multispecx import mapbuilder
 
 work_dir = "/Users/akanane/Documents/Research/ester"
-transform = [['center',3],['rotate',3,4,'x'],['rotate',3,1,'y']]
+transform = [['center',2],['rotate',2,6,'x'],['rotate',2,1,'y']]
 basis="6-311++G(d,p)"
 nframes=2
 cut_off1=5.0
 cut_off2=8.5
 
-s = mapbuilder.Mapbuilder(itp=["etoac.itp","tip4p.itp"],top="etoac.top",gro="prod.gro",method="B3LYP",basis=basis,vib="normal",nframes=nframes,xyz=["etoac.xyz","water.xyz"],software="Gaussian",wdir=work_dir,xtc="prod.xtc",transform=transform,solv_ref_atom="OW",solu_ref_atom="C4",ncores=40,cut_off1=cut_off1)
+s = mapbuilder.Mapbuilder(itp=["etoac_tip3p.top"],top="etoac_tip3p.top",gro="etoac_tip3p.gro",method="B3LYP",basis=basis,vib="normal",nframes=nframes,xyz=["etoac.xyz","water.xyz"],software="Gaussian",wdir=work_dir,xtc="etoac_tip3p.xtc",transform=transform,solv_ref_atom="O",solu_ref_atom="C4",ncores=40,cut_off1=cut_off1)
 s.createJobs()
 
