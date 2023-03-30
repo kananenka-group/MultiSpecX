c643dd83cb10c954939242cb1fe11c74599309d6
Alexei Kananenka <akanane@udel.edu>
2023-03-30 12:39:30 -0500

    adjusted parameters of map test

--- tests/map/run.py
+++ tests/map/run.py
@@ -10,6 +10,6 @@ nframes=2
 cut_off1=5.0
 cut_off2=8.5
 
-s = mapbuilder.Mapbuilder(itp=["etoac_tip3p.top"],top="etoac_tip3p.top",gro="etoac_tip3p.gro",method="B3LYP",basis=basis,vib="normal",nframes=nframes,xyz=["etoac.xyz","water.xyz"],software="Gaussian",wdir=work_dir,xtc="etoac_tip3p.xtc",transform=transform,solv_ref_atom="O",solu_ref_atom="C4",ncores=40,cut_off1=cut_off1)
+s = mapbuilder.Mapbuilder(itp=["etoac_tip3p.top"],top="etoac_tip3p.top",gro="etoac_tip3p.gro",method="B3LYP",basis=basis,vib="normal",nframes=nframes,xyz=["etoac.xyz","water.xyz"],software="Gaussian",wdir=work_dir,xtc="etoac_tip3p.xtc",transform=transform,solv_ref_atom="O",solu_ref_atom="O2",ncores=40,cut_off1=cut_off1,cut_off2=cut_off2)
 s.createJobs()
 
