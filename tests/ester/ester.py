c535f9c46a81dda04b0aec419d193d16f847656d
Alexei Kananenka <akanane@udel.edu>
2023-03-30 19:13:10 -0500

    added new parameters for the ester test

--- tests/ester/ester.py
+++ tests/ester/ester.py
@@ -3,10 +3,12 @@ sys.path.append("/Users/akanane/Documents/Software/MultiSpecX/")
 from multispecx import spectrum
 from multispecx import ester
 
-itp_files = ["topol_Protein.itp","topol_Protein2.itp","topol_Protein3.itp","topol_Protein4.itp","topol_Other5.itp","topol_Other6.itp","topol_Other7.itp","tip3p.itp"]
+itp_files = ["topol_Protein.itp","topol_Protein2.itp","topol_Protein3.itp","topol_Protein4.itp","topol_Other5.itp","topol_Other6.itp","topol_Other7.itp","spc.itp"]
 ester_unit=['C','O','OE','CA']
+transform=[['center','C'],['rotate','C','O','x'],['rotate','C','CA','y']]
+xtc = "/Users/akanane/Documents/Research/EsterKcsA/1/20ps.xtc"
 
-s = ester.Ester(itp=itp_files,top="topol.top",gro="conf.gro",ester_unit=ester_unit)
+s = ester.Ester(itp=itp_files,top="topol.top",gro="20ps.gro",ester_unit=ester_unit,transform=transform,xtc=xtc)
 s.generateHamiltonian()
 
 
